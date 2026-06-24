06bfe22bee23c3580e0211f57f98f895aca2a83b
Bharath <52877455+Bharath2000-palle@users.noreply.github.com>
2026-06-24 10:55:41 +0530

    Update app.py
    updated app.py file to demo working of agents

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def stress_detection():
     
     return render_template('final.html', prediction_text='{}'.format(pred))
 
+api_access_token = 'eryjiw_wjijir'
 @app.route('/predict_api',methods=['POST']) 
 def predict_api():
     '''
